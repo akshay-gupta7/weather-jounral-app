d2a5ff6b883d296e262672a5143495040a1f1b40
akshay <gravity.akshay@gmail.com>
2022-04-10 23:25:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
-// Express to run server and routes
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
+
+// TODO-Express to run server and routes
 const express = require('express');
 
-// Start up an instance of app
+// TODO-Start up an instance of app
+
 const app = express();
 
 /* Dependencies */
 const bodyParser = require('body-parser')
-/* Middleware*/
-//Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-// Cors for cross origin allowance
+
+// TODO-Cors for cross origin allowance
 const cors = require('cors');
-app.use(cors());
+
+
+/* Initializing the main project folder */
+app.use(express.static('website'));
+
+const port = 3000;

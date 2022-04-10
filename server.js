d2488e4cb56711e4d02f0932737def6e4435f5cc
akshay <gravity.akshay@gmail.com>
2022-04-10 23:30:47 +0530

    Add port and listening function

--- server.js
+++ server.js
@@ -21,3 +21,10 @@ const cors = require('cors');
 app.use(express.static('website'));
 
 const port = 3000;
+
+const server = app.listen(port, listening);
+
+function listening(){
+    console.log("server running");
+    console.log(`running on localhost" $(port)`);
+}

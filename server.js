9ace0b64bd6fdba724779ba06eebdd3d6f09e4b8
akshay <gravity.akshay@gmail.com>
2022-04-10 23:37:07 +0530

    Add package json

--- server.js
+++ server.js
@@ -26,5 +26,5 @@ const server = app.listen(port, listening);
 
 function listening(){
     console.log("server running");
-    console.log(`running on localhost" $(port)`);
+    console.log(`running on localhost ${port}`);
 }
